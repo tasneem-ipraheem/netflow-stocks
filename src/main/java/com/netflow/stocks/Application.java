405fe91579f4ad93624bab10a659256bddb84be0
Edvinas Veretinskas <edvinas.veretinskas@gmail.com>
2015-10-04 01:03:23 +0100

    Introducing basic and environment application properties
     * default application.properties
     * environment specific netflow-stocks.properties

--- src/main/java/com/netflow/stocks/Application.java
+++ src/main/java/com/netflow/stocks/Application.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
 @EnableJpaRepositories("com.netflow.stocks")
+@PropertySource(value = "file:${netflow.stocks.home}\\netflow-stocks.properties")
 public class Application {
 
     public static void main(String[] args) {
