1210d39d0edb7d47923a8163116f577bb456d88e
Edvinas Veretinskas <edvinas.veretinskas@gmail.com>
2015-11-16 20:40:15 +0000

    Adding Swagger UI for REST API documentation

--- src/main/java/com/netflow/stocks/api/StocksApi.java
+++ src/main/java/com/netflow/stocks/api/StocksApi.java
@@ -1,19 +1,26 @@
 package com.netflow.stocks.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Collection;
 
 
+@Api
 @Path("/")
 public interface StocksApi {
 
+
+    @ApiOperation(value = "aaa")
     @GET
     @Path("/stock/{stockId}")
     @Encoded
     @Produces(MediaType.APPLICATION_JSON)
     StockDto getStockBySymbol(@PathParam("stockId") String stockId);
 
+    @ApiOperation("bbb")
     @GET
     @Path("/lookup")
     @Encoded
--- src/main/java/com/netflow/stocks/rest/JerseyConfig.java
+++ src/main/java/com/netflow/stocks/rest/JerseyConfig.java
@@ -2,7 +2,11 @@ package com.netflow.stocks.rest;
 
 import com.netflow.stocks.service.StocksService;
 import com.netflow.stocks.statistics.StatsService;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.wadl.internal.WadlResource;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.ApplicationPath;
@@ -12,6 +16,25 @@ import javax.ws.rs.ApplicationPath;
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
+        registerEndpoints();
+        configureSwagger();
+    }
+
+    private void configureSwagger() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("2.0");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("localhost:80");
+        beanConfig.setBasePath("/stocks");
+        beanConfig.setResourcePackage("com.netflow.stocks");
+        beanConfig.setPrettyPrint(true);
+        beanConfig.setScan(true);
+    }
+
+    private void registerEndpoints() {
+        register(ApiListingResource.class);
+        register(SwaggerSerializers.class);
+        register(WadlResource.class);
         register(StocksService.class);
         register(StatsService.class);
     }
